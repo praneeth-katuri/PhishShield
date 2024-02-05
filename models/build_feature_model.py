779ba900048322b72b3f4d074b9754c4f8e61f20
praneeth-katuri <praneethkaturi@outlook.com>
2024-02-05 19:21:43 +0530

    feat(sampling): Apply SMOTE to handle class imbalance

--- models/build_feature_model.py
+++ models/build_feature_model.py
@@ -5,6 +5,7 @@ from sklearn import metrics
 from sklearn.svm import SVC
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import GridSearchCV
+from imblearn.over_sampling import SMOTE
 
 def load_data(file_path):
     data = pd.read_csv(file_path)
@@ -12,12 +13,18 @@ def load_data(file_path):
     y = data["class"]
     return X, y
 
+def upsample_data(X_train, y_train):
+    smote = SMOTE(random_state=42)
+    X_res, y_res = smote.fit_resample(X_train, y_train)
+    return X_res, y_res
+
 def split_data(X, y):
     return train_test_split(X, y, test_size=0.2, random_state=42)
 
 def main():
     X, y = load_data("datafiles/dataset_for_feature_model.csv")
-    X_train, X_test, y_train, y_test = split_data(X, y)
+    X_train, y_train = upsample_data(X_train, y_train)
+
 
     param_grids = {
     "Logistic Regression": {'C': [1, 10]},
