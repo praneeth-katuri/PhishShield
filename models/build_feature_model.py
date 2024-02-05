ffaaee3341a5092a101b87842c2d0ef650eb681a
praneeth-katuri <praneethkaturi@outlook.com>
2024-02-05 10:39:12 +0530

    feat(gridsearch): Optimize models with basic GridSearchCV

--- models/build_feature_model.py
+++ models/build_feature_model.py
@@ -4,6 +4,7 @@ from sklearn.linear_model import LogisticRegression
 from sklearn import metrics
 from sklearn.svm import SVC
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import GridSearchCV
 
 def load_data(file_path):
     data = pd.read_csv(file_path)
@@ -18,11 +19,17 @@ def main():
     X, y = load_data("datafiles/dataset_for_feature_model.csv")
     X_train, X_test, y_train, y_test = split_data(X, y)
 
+    param_grids = {
+    "Logistic Regression": {'C': [1, 10]},
+    "Support Vector Machine": {'C': [1, 10]},
+    "Random Forest": {'n_estimators': [100, 200]}
+}
+
     models = {
-        "Logistic Regression": LogisticRegression(),
-        "Support Vector Machine": SVC(),
-        "Random Forest": RandomForestClassifier()
-    }
+    "Logistic Regression": GridSearchCV(LogisticRegression(), param_grids["Logistic Regression"], cv=3),
+    "Support Vector Machine": GridSearchCV(SVC(), param_grids["Support Vector Machine"], cv=3),
+    "Random Forest": GridSearchCV(RandomForestClassifier(), param_grids["Random Forest"], cv=3)
+}
 
     for name, model in models.items():
         model.fit(X_train, y_train)
