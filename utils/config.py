926635440a67475ec0ca88824198fafadc1a7be4
praneeth-katuri <praneethkaturi@outlook.com>
2024-02-18 19:12:11 +0530

    refactor: moved configurations to dedicated config file

--- utils/config.py
+++ utils/config.py
@@ -1,9 +1,8 @@
-# backend/utils/config.py
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
-
-FLASK_SECRET_KEY = os.getenv("SECRET_KEY")
-RECAPTCHA_SECRET_KEY = os.getenv("RECAPTCHA_SECRET_KEY")
-RECAPTCHA_SITE_KEY = os.getenv("RECAPTCHA_SITE_KEY")
+class Config:
+    SECRET_KEY = os.getenv("SECRET_KEY")
+    RECAPTCHA_SECRET_KEY = os.getenv("RECAPTCHA_SECRET_KEY")
+    RECAPTCHA_SITE_KEY = os.getenv("RECAPTCHA_SITE_KEY")
